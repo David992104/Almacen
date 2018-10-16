cbfff5af0b3ee05266d5666e8787f90a52977f95
David990421 <david.osornio99@gmail.com>
2018-10-15 21:41:17 -0500

    Esqueleto Principal

---
+++ src/almacen/Almacen.java
@@ -0,0 +1,18 @@
+package almacen;
+
+import almcenView.LoginView;
+/**
+ *
+ * @author djoso
+ */
+public class Almacen {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        LoginView login = new LoginView();
+        login.setVisible(true);
+    }
+
+}
