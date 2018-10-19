827949b90b0d33377270d271b8e8b7347b181edf
David990421 <david.osornio99@gmail.com>
2018-10-19 09:53:38 -0500

    Actualizacion DB

--- AlmacenDB.sql
+++ AlmacenDB.sql
@@ -1,16 +1,17 @@
+
 create database Almacen;
 use Almacen;
 drop table users;
 
-create table users(
-idUsuer int auto_increment key,
-nameUser varchar (20) not null,
-lastNameUser varchar(30) not null,
-emailUser varchar(50) not null,
-passUser varchar(50) not null, 
-imagenUser longblob
-);
-
+#create table users(
+#idUsuer int auto_increment key,
+#nameUser varchar (20) not null,
+#lastNameUser varchar(30) not null,
+#emailUser varchar(50) not null,
+#passUser varchar(50) not null, 
+#imagenUser longblob
+#);
+drop table material;
 create table material(
 idMaterial int key,
 posicionMaterial int (10),
@@ -23,7 +24,7 @@ maximaCantidad int(10),
 minimaCantidad int(10),
 imagenMaterial longblob
 );
-
+ drop table salidasMaterial;
 create table salidasMaterial(
 idSalida int auto_increment key,
 idMaterial int ,
@@ -38,6 +39,7 @@ usuario varchar(50),
 imagenMaterial longblob
 );
 
+drop table entradasmaterial;
 create table entradasMaterial(
 idEntrada int auto_increment key,
 idMaterial int ,
@@ -50,4 +52,17 @@ cuantosSalen int(20),
 fecha date,
 usuario varchar(50),
 imagenMaterial longblob
+);
+
+create table categoria(
+idCategoria int auto_increment key,
+nombreCategoria varchar (20),
+descripcion varchar (100)
+);
+
+create table tipo(
+idTipo int auto_increment key,
+idCategoria int (10),
+nombreTipo varchar (20),
+descripcion varchar (100)
 );
